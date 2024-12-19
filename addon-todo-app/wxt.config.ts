90ad5445d2828debbb92726fa2ffc88b2eec6083
witrellesp <william.trelles1@eduvaud.ch>
2024-12-19 10:31:12 +0100

    correction erreur de build

--- addon-todo-app/wxt.config.ts
+++ addon-todo-app/wxt.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
-  entrypointsDir: "./entrypoints/popup",
-  outDir: "./build/firefox",
   manifest: {
     name: "Todo App",
     version: "0.0.4",
