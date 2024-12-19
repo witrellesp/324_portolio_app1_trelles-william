9d463bed2ac1672425e1d775fbe59c2db8c2aa33
witrellesp <william.trelles1@eduvaud.ch>
2024-12-19 10:42:11 +0100

    correction wxt.configs.ts

--- addon-todo-app/wxt.config.ts
+++ addon-todo-app/wxt.config.ts
@@ -1,19 +1,25 @@
 import { defineConfig } from "wxt";
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: "chrome",
+  extensionApi: "chrome", // Cambia a 'firefox' si es necesario
+  entrypointsDir: "./entrypoints/popup", // Define el directorio de entrypoints
+  outDir: "./.output/firefox", // Define el directorio de salida para Firefox
   manifest: {
     name: "Todo App",
-    version: "0.0.4",
-    description:
-      "Cet addon permet de gérer une liste de tâche à faire et de la partager.",
+    version: "0.0.5",
+    description: "Addon para manejar tareas pendientes",
     permissions: ["identity"],
-    host_permissions: [],
     browser_specific_settings: {
       gecko: {
-        id: "{47176d92-f9c3-4cb8-ab6f-eef99bdbd667}",
+        id: "{addon-id}",
         strict_min_version: "109.0",
+      },
+    },
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        input: "./entrypoints/popup/index.html", // Especifica el archivo principal
       },
     },
   },
