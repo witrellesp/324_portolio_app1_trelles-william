cc3a931d0c2be6f4c5441b62f0371fce79c253c3
witrellesp <william.trelles1@eduvaud.ch>
2024-12-19 10:47:08 +0100

    correction wxt.configs.ts

--- addon-todo-app/wxt.config.ts
+++ addon-todo-app/wxt.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "wxt";
 
 export default defineConfig({
-  extensionApi: "chrome", // Cambia a 'firefox' si es necesario
-  entrypointsDir: "./entrypoints/popup", // Define el directorio de entrypoints
-  outDir: "./.output/firefox", // Define el directorio de salida para Firefox
+  extensionApi: "chrome",
+  entrypointsDir: "./entrypoints/popup", // Define dónde están los entrypoints
+  outDir: "./.output/firefox", // Define el directorio de salida
   manifest: {
     name: "Todo App",
     version: "0.0.5",
@@ -13,13 +13,6 @@ export default defineConfig({
       gecko: {
         id: "{addon-id}",
         strict_min_version: "109.0",
-      },
-    },
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        input: "./entrypoints/popup/index.html", // Especifica el archivo principal
       },
     },
   },
