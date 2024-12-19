0bb22d766532fb986c650f96f945883f858c5016
witrellesp <william.trelles1@eduvaud.ch>
2024-12-19 10:04:18 +0100

    correction erreur de build

--- addon-todo-app/wxt.config.ts
+++ addon-todo-app/wxt.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
+  extensionApi: "chrome",
+  entrypointsDir: "./entrypoints/popup",
+  outDir: "./build/firefox",
   manifest: {
     name: "Todo App",
     version: "0.0.4",
-    description: "Cet addon permet de gérer une liste de tâche à faire et de la partager.",
+    description:
+      "Cet addon permet de gérer une liste de tâche à faire et de la partager.",
     permissions: ["identity"],
     host_permissions: [],
     browser_specific_settings: {
-        gecko: {
-            id: "{47176d92-f9c3-4cb8-ab6f-eef99bdbd667}",
-            strict_min_version: "109.0"
-        }
+      gecko: {
+        id: "{47176d92-f9c3-4cb8-ab6f-eef99bdbd667}",
+        strict_min_version: "109.0",
+      },
     },
   },
 });
