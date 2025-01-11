0586b33698e8423ddebb8a14f1e6d183d6df4f1b
witrellesp <william.trelles1@eduvaud.ch>
2025-01-11 18:37:41 +0100

    version of add-on changed in wxt.config

--- addon-todo-app/wxt.config.ts
+++ addon-todo-app/wxt.config.ts
@@ -6,14 +6,14 @@ export default defineConfig({
   outDir: "./.output/firefox",
   manifest: {
     name: "Todo App",
-    version: "0.0.6",
+    version: process.env.VERSION || "0.0.6",
     description:
       "Cet addon permet de gérer une liste de tâche à faire et de la partager.",
     permissions: ["identity"],
     host_permissions: [],
     browser_specific_settings: {
       gecko: {
-        id:  process.env.FIREFOX_ADDON_ID,
+        id:  process.env.FIREFOX_ADDON_ID || "default@email.com",
         strict_min_version: "109.0",
       },
     },
